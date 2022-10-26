578cc432a7f486ce0f85439c81897fb05d43df18
oumaimalharrak <oumaimalharrak96@gmail.com>
2022-10-26 16:42:15 +0100

    android permissions

--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -11,154 +11,52 @@ import {
 import CallActionBox from '../../components/CallActionBox';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {useNavigation, useRoute} from '@react-navigation/core';
-import {Voximplant} from 'react-native-voximplant';
 
-const permissions = [
+const permissions =[
   PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-  PermissionsAndroid.PERMISSIONS.CAMERA,
-];
+  PermissionsAndroid.PERMISSIONS.CAMERA
+]
 
 const CallingScreen = () => {
-  const [permissionGranted, setPermissionGranted] = useState(false);
-  const [callStatus, setCallStatus] = useState('Initializing...');
-  const [localVideoStreamId, setLocalVideoStreamId] = useState('');
-  const [remoteVideoStreamId, setRemoteVideoStreamId] = useState('');
-
+  const [permissionGranted, setPermissionGranted] = useState(flase)
+ 
   const navigation = useNavigation();
   const route = useRoute();
 
-  const {user, call: incomingCall, isIncomingCall} = route?.params;
-
-  const voximplant = Voximplant.getInstance();
-
-  const call = useRef(incomingCall);
-  const endpoint = useRef(null);
+  
 
   const goBack = () => {
     navigation.pop();
   };
 
-  useEffect(() => {
-    const getPermissions = async () => {
-      const granted = await PermissionsAndroid.requestMultiple(permissions);
+  useEffect(()=>{
+    const getPermission = async () =>{
+      const granted =await PermissionsAndroid.requestMultiple(permissions);
       const recordAudioGranted =
-        granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] === 'granted';
-      const cameraGranted =
-        granted[PermissionsAndroid.PERMISSIONS.CAMERA] === 'granted';
-      if (!cameraGranted || !recordAudioGranted) {
-        Alert.alert('Permissions not granted');
-      } else {
-        setPermissionGranted(true);
+      granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] === 'granted';
+      const cameraGranted= 
+      granted[PermissionsAndroid.PERMISSIONS.CAMERA] === 'granted';
+      if(!cameraGranted || !recordAudioGranted){
+        Alert.alert('permissions not granted')
+      }else{
+        setPermissionGranted();
       }
     };
-
-    if (Platform.OS === 'android') {
-      getPermissions();
-    } else {
-      setPermissionGranted(true);
+    if(Platform.OS === 'android'){
+      getPermission()
     }
-  }, []);
+  },[])
 
-  useEffect(() => {
-    if (!permissionGranted) {
-      return;
-    }
-
-    const callSettings = {
-      video: {
-        sendVideo: true,
-        receiveVideo: true,
-      },
-    };
-
-    const makeCall = async () => {
-      call.current = await voximplant.call(user.user_name, callSettings);
-      subscribeToCallEvents();
-    };
-
-    const answerCall = async () => {
-      subscribeToCallEvents();
-      endpoint.current = call.current.getEndpoints()[0];
-      subscribeToEndpointEvent();
-      call.current.answer(callSettings);
-    };
-
-    const subscribeToCallEvents = () => {
-      call.current.on(Voximplant.CallEvents.Failed, callEvent => {
-        showError(callEvent.reason);
-      });
-      call.current.on(Voximplant.CallEvents.ProgressToneStart, callEvent => {
-        setCallStatus('Calling...');
-      });
-      call.current.on(Voximplant.CallEvents.Connected, callEvent => {
-        setCallStatus('Connected');
-      });
-      call.current.on(Voximplant.CallEvents.Disconnected, callEvent => {
-        navigation.navigate('Contacts');
-      });
-      call.current.on(
-        Voximplant.CallEvents.LocalVideoStreamAdded,
-        callEvent => {
-          setLocalVideoStreamId(callEvent.videoStream.id);
-        },
-      );
-      call.current.on(Voximplant.CallEvents.EndpointAdded, callEvent => {
-        endpoint.current = callEvent.endpoint;
-        subscribeToEndpointEvent();
-      });
-    };
-
-    const subscribeToEndpointEvent = async () => {
-      endpoint.current.on(
-        Voximplant.EndpointEvents.RemoteVideoStreamAdded,
-        endpointEvent => {
-          setRemoteVideoStreamId(endpointEvent.videoStream.id);
-        },
-      );
-    };
-
-    const showError = reason => {
-      Alert.alert('Call failed', `Reason: ${reason}`, [
-        {
-          text: 'Ok',
-          onPress: navigation.navigate('Contacts'),
-        },
-      ]);
-    };
-
-    if (isIncomingCall) {
-      answerCall();
-    } else {
-      makeCall();
-    }
-
-    return () => {
-      call.current.off(Voximplant.CallEvents.Failed);
-      call.current.off(Voximplant.CallEvents.ProgressToneStart);
-      call.current.off(Voximplant.CallEvents.Connected);
-      call.current.off(Voximplant.CallEvents.Disconnected);
-    };
-  }, [permissionGranted]);
-
-  const onHangupPress = () => {
-    call.current.hangup();
-  };
-
+ 
   return (
     <View style={styles.page}>
       <Pressable onPress={goBack} style={styles.backButton}>
         <Ionicons name="chevron-back" color="white" size={25} />
       </Pressable>
 
-      <Voximplant.VideoView
-        videoStreamId={remoteVideoStreamId}
-        style={styles.remoteVideo}
-      />
+     
 
-      <Voximplant.VideoView
-        videoStreamId={localVideoStreamId}
-        style={styles.localVideo}
-      />
+     
 
       <View style={styles.cameraPreview}>
         <Text style={styles.name}>{user?.user_display_name}</Text>
--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -5,48 +5,52 @@ import { APP_NAME, ACC_NAME } from '../../constant';
 import { useNavigation } from '@react-navigation/native';
 const LoginScreen = () => {
 
-    const [userName, setUserName] = useState('');
+    const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
 
     const voximplant = Voximplant.getInstance();
     const navigation = useNavigation();
 
+  
+
     useEffect(() => {
-      const connect = async ()=> {
-        const status = await voximplant.getClientState();
-        if(status === voximplant.ClientState.DISCONNECTED){
+        const connect = async () => {
+          const status = await voximplant.getClientState();
+          if (status === Voximplant.ClientState.DISCONNECTED) {
             await voximplant.connect();
-        }else if(status === Voximplant.ClientState.LOGGED_IN){
+          } else if (status === Voximplant.ClientState.LOGGED_IN) {
             redirectHome();
-        }
-      }
-      connect();
-    }, [])
-
-    const signIn =async ()=>{
-        try{
-            const fqUsername = `${userName}@${APP_NAME}.${ACC_NAME}.voximplant.com`
-            await voximplant.login();
-            redirectHome();
-        }catch (e){
-            Alert.alert(e.name, `Error code : ${e.code}`)
-
-        }
-    }
+          }
+        };
     
-    const redirectHome =() =>{
+        connect();
+      }, []);
+    
+      const signIn =  () => {
+        
+            const fqUsername = `${username}@${APP_NAME}.${ACC_NAME}.voximplant.com`;
+           voximplant.login(fqUsername, password );
+           redirectHome();
+        // } 
+        // catch (e) {
+        //   console.log(e);
+        //   Alert.alert(e.name, `Error code: ${e.code}`);
+        // }
+      };
+    
+      const redirectHome = () => {
         navigation.reset({
-            index : 0,
-            routes :[{
-                name :'Contacts'
+          index: 0,
+          routes: [
+            {
+              name: 'Contacts',
             },
-                
-            ]
-        })
-    }
+          ],
+        });
+      };
   return (
     <View style={styles.page}>
-      <TextInput value={userName} onChangeText={setUserName} placeholder="User name" style={styles.input}  />
+      <TextInput value={username} onChangeText={setUserName} placeholder="User name" style={styles.input}  />
       <TextInput value={password} onChangeText={setPassword}  placeholder="Password" style={styles.input} secureTextEntry />
 
       <Pressable style={styles.button} onPress={signIn}>
@@ -69,7 +73,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         padding: 10,
         marginVertical: 10,
-        borderRadius: 8
+        borderRadius: 8,
+        color: 'black'
     },
     button:{
         backgroundColor: '#5a70bf',
